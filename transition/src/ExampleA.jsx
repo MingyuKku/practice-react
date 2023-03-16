8b42eb356e11ff97516e07be86037af10591d6f1
MingyuKku <gang33431@naver.com>
2023-03-16 17:42:47 +0900

    트렌지션 테스트

---
+++ transition/src/ExampleA.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import { CSSTransition } from 'react-transition-group';
+// import CSSTransition from 'react-transition-group/CSSTransition';
+
+const ExampleA = () => {
+  const [ isEnter, setIsEnter ] = useState(true); // 처음 값이 true일 때만 appear 적용할 수 있음
+  
+  return (
+    <div className='container'>
+      <button onClick={() => {setIsEnter((v) => !v)}}>트렌지션</button>
+      <CSSTransition
+        in={ isEnter }
+        appear={ true }
+        timeout={ {appear: 300, enter: 300, exit: 300} }
+        classNames="myTrans"
+      >
+        
+        <p className='my-text'>애니메이션 요소 { isEnter ? '엔터':'종료' }</p>
+      </CSSTransition>
+    </div>
+  )
+}
+
+export default ExampleA;
